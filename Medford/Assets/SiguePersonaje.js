8e3af774f094531631e53c2ae8f406aa342ca958
jeanca07b <jcarlosariasb@hotmail.com>
2015-12-18 21:58:05 -0600

    Proyecto inicial
    Así empieza el juego

---
+++ Medford/Assets/SiguePersonaje.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var jugador : Transform;
+
+function Start () {
+	jugador = GameObject.FindGameObjectWithTag("Player").transform;
+}
+
+function Update () {
+	transform.position.x = jugador.position.x;
+	transform.position.y = jugador.position.y + 2;
+	
+}
